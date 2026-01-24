1020bfb2e0c1899ede91a9dfc8f1cbe13aff9bb6
gustavogabana <gustavo_97@protonmail.com>
2026-01-24 16:18:30 -0300

    feat: errorComponent

--- src/routes/posts/$postId.tsx
+++ src/routes/posts/$postId.tsx
@@ -3,12 +3,14 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/posts/$postId")({
     component: RouteComponent,
     loader: async ({ params }) => {
+        throw new Error();
         await new Promise((resolve) => setTimeout(resolve, 1000));
         return {
             postId: params.postId
         }
     },
-    pendingComponent: () => <div>Loading...</div>
+    pendingComponent: () => <div>Loading...</div>,
+    errorComponent: () => <div>Error</div>
 });
 
 function RouteComponent() {
