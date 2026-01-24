fd480b0d886aa4b8568d70b9dc8f2d600032c6b0
gustavogabana <gustavo_97@protonmail.com>
2026-01-24 16:17:38 -0300

    feat: pending component

--- src/routes/posts/$postId.tsx
+++ src/routes/posts/$postId.tsx
@@ -3,10 +3,12 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/posts/$postId")({
     component: RouteComponent,
     loader: async ({ params }) => {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
         return {
             postId: params.postId
         }
-    }
+    },
+    pendingComponent: () => <div>Loading...</div>
 });
 
 function RouteComponent() {
