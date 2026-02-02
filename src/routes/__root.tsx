fde6f96e5a7115f3bf9ef66152128d29040da2a8
gustavogabana <gustavo_97@protonmail.com>
2026-02-01 21:27:22 -0300

    chore: reorganazing main root file

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,36 +1,16 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import Navbar from "@/components/Navbar";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 const RootLayout = () => (
   <>
-    <div className="p-2 flex gap-2">
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>
-      <Link to="/about" className="[&.active]:font-bold">
-        About
-      </Link>
-      <Link 
-        to="/posts" 
-        search={{ query: "post 1" }}
-        className="[&.active]:font-bold">
-        Post
-      </Link>
-      <Link to="/form" className="[&.active]:font-bold">
-        Form
-      </Link>
-      <Link to="/card" className="[&.active]:font-bold">
-        Card
-      </Link>
-      <Link to="/todos" className="[&.active]:font-bold">
-        Todos
-      </Link>
-      <Link to="/virtual" className="[&.active]:font-bold">
-        Virtual
-      </Link>
-    </div>
+    <header>
+      <Navbar />
+    </header>
     <hr />
-    <Outlet />
+    <main>
+      <Outlet />
+    </main>
     <TanStackRouterDevtools />
   </>
 );
